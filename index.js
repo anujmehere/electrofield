38379531b32805dd1f669aed31f8698245fd0d80
anujmehere <mehereanuj570@gmail.com>
2021-11-14 15:39:00 +0530

    4

--- index.js
+++ index.js
@@ -33,7 +33,19 @@
 //     });
     
 // }
-
+function movetoken()
+{
+  document.getElementById("tkn1").value=document.getElementById("tkn2").value;
+  document.getElementById("tkn2").value=document.getElementById("tkn3").value;
+  document.getElementById("tkn3").value=document.getElementById("tkn4").value;
+  document.getElementById("tkn4").value=document.getElementById("tkn5").value;
+  document.getElementById("tkn5").value=document.getElementById("tkn6").value;
+  document.getElementById("tkn6").value=document.getElementById("tkn7").value;
+  document.getElementById("tkn7").value=document.getElementById("tkn8").value;
+  
+  document.getElementById("tkn8").value=document.getElementById("newtkn").value;
+  
+}
 
 // function clearalltoken() {
 //     clralltkn = document.getElementById("clralltkn");
@@ -49,34 +61,19 @@
 //     tknen.value = 0;
 //     });
 // }
+function clearalltoken() {
+    document.getElementById("tkn1").value=0;
+    document.getElementById("tkn2").value=0;
+    document.getElementById("tkn3").value=0;
+    document.getElementById("tkn4").value=0;
 
+    document.getElementById("tkn5").value=0;
+    document.getElementById("tkn6").value=0;
+    document.getElementById("tkn7").value=0;
+    document.getElementById("tkn8").value=0;
+}
 
 window.addEventListener("load", ()=>{
-
-  function movetoken()
-{
-  document.getElementById("tkn1").value=document.getElementById("tkn2").value;
-  document.getElementById("tkn2").value=document.getElementById("tkn3").value;
-  document.getElementById("tkn3").value=document.getElementById("tkn4").value;
-  document.getElementById("tkn4").value=document.getElementById("tkn5").value;
-  document.getElementById("tkn5").value=document.getElementById("tkn6").value;
-  document.getElementById("tkn6").value=document.getElementById("tkn7").value;
-  document.getElementById("tkn7").value=document.getElementById("tkn8").value;
-  
-  document.getElementById("tkn8").value=document.getElementById("newtkn").value;
-  
-}
-function clearalltoken() {
-  document.getElementById("tkn1").value=0;
-  document.getElementById("tkn2").value=0;
-  document.getElementById("tkn3").value=0;
-  document.getElementById("tkn4").value=0;
-
-  document.getElementById("tkn5").value=0;
-  document.getElementById("tkn6").value=0;
-  document.getElementById("tkn7").value=0;
-  document.getElementById("tkn8").value=0;
-}
 
 const loader = document.querySelector(".loader")
   loader.classList.add("hidden")
