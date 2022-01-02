cd664376d2f0158ccb53436fc1a7449c7a6718d6
anujmehere <mehereanuj570@gmail.com>
2022-01-02 15:13:23 +0530

    tokendisplay

---
+++ tokendiplay/js/index.js
@@ -0,0 +1,60 @@
+// function movetoken()
+// {
+//   document.getElementById("tkn1").value=document.getElementById("tkn2").value;
+//   document.getElementById("tkn2").value=document.getElementById("tkn3").value;
+//   document.getElementById("tkn3").value=document.getElementById("tkn4").value;
+//   document.getElementById("tkn4").value=document.getElementById("tkn5").value;
+//   document.getElementById("tkn5").value=document.getElementById("tkn6").value;
+//   document.getElementById("tkn6").value=document.getElementById("tkn7").value;
+//   document.getElementById("tkn7").value=document.getElementById("tkn8").value;
+  
+//   document.getElementById("tkn8").value=document.getElementById("newtkn").value;
+  
+// }
+
+function clearalltoken() {
+    document.getElementById("tkn1").value=0;
+    document.getElementById("tkn2").value=0;
+    document.getElementById("tkn3").value=0;
+    document.getElementById("tkn4").value=0;
+
+    document.getElementById("tkn5").value=0;
+    document.getElementById("tkn6").value=0;
+    document.getElementById("tkn7").value=0;
+    document.getElementById("tkn8").value=0;
+}
+
+window.addEventListener("load", ()=>{
+
+const loader = document.querySelector(".loader")
+  loader.classList.add("hidden")
+
+
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("sw.js")
+      .then((registration) => console.log("yoo, Service worker registered"))
+      .catch((error) => console.log("Service worker not registered", error));
+
+
+  }
+  
+})
+
+function myFunction() {
+  let details = navigator.userAgent;
+  let regexp = /android|iphone|kindle|ipad/i;
+  let isMobileDevice = regexp.test(details);
+  if (isMobileDevice) {
+    document.getElementById("dtype").value="Mobile";
+  } 
+  else {
+    document.getElementById("dtype").value="Desktop";
+    }
+    
+  }
+
+  //Bluetooth
+  
+ 
+  
