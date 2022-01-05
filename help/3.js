34899b8b9c366fee2a513dd43fc94a46017cc313
anujmehere <mehereanuj570@gmail.com>
2022-01-05 12:51:37 +0530

    1

--- help/3.js
+++
@@ -1,9 +0,0 @@
-function getFirstParamFromLocation() {
-  var pair = window.location.search.substring(1);
-  var parts = pair.split("=");
-  return parts[1];
-}
-var nHeight = getFirstParamFromLocation();
-try {
-  window.top.resizeIframeHeight(height);
-} catch (e) {}
